23aa43ac2dc9041fd4ea0732afdfb235046a2ca4
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-27 04:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(currentLine, personName){
   currentLine.push(personName)
   return `Welcome, ${personName}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Now serving ${katzDeliLine.shift()}`
+  }
+}
