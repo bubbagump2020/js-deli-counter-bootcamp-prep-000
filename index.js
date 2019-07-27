2c46e9c9756fdec90cfbac1629157367ce73adf5
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-27 04:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeliLine = []
 
 function takeANumber(currentLine, personName){
-  katzDeliLine.push(personName)
+  currentLine.push(personName)
+  return `Welcome ${personName}. You are number ${currentLine.length} in line.`
 }
