70050bfb787650666b1a7d5ddb6c4a211eb53dd2
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-27 04:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  var currentLine = []
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   } 
