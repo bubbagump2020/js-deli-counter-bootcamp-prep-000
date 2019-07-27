c18c7c577827f2c13b81d7178231f3bffe5b4f5d
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-27 04:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine){
     for (var i = 1; i < katzDeliLine.length; i++){
       currentLine.push(i)
     }
-    return `The line is currently: ${currentLine.concat(kazDeliLine.join(", "))}`
+    return `The line is currently: ${currentLine.concat(katzDeliLine.join(", "))}`
   }
   
   
